dc37638796fdd92d37ea65c6629c59d918d95283
Alecto Irene Perez <alecto.perez@gmail.com>
2025-09-15 00:29:08 -0600

    cln objects.cpp

--- examples/src/objects.cpp
+++ examples/src/objects.cpp
@@ -3,8 +3,7 @@
 #include <string>
 #include <vector>
 
-using bytes = std::vector<std::byte>;
-
+using bytes      = std::vector<std::byte>;
 using string_map = std::map<std::string, std::string>;
 
 struct my_object {
@@ -14,11 +13,13 @@ struct my_object {
 };
 
 int main() {
-    my_object m{bytes(10),
-                bytes(100),
-                {
-                    {"key1", "Hello, world"},
-                    {"key2", "The quick brown fox jumps over the lazy dogs"},
-                    {"key3", "Have a great CppCon!"},
-                }};
+    my_object m{
+        bytes(10),
+        bytes(100),
+        string_map{
+            {"key1", "Hello, world"},
+            {"key2", "The quick brown fox jumps over the lazy dogs"},
+            {"key3", "Have a great CppCon!"},
+        },
+    };
 }
