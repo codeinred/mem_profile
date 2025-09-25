4e8aafbea00b63bf987c2871e0e159bb4e11f227
Alecto Irene Perez <alecto.perez@gmail.com>
2025-09-25 01:19:40 -0400

    add mp::debug_write - useful for dumping to stdout without buffering

--- mp/runtime/include/mem_profile/io.h
+++ mp/runtime/include/mem_profile/io.h
@@ -7,7 +7,12 @@
 ////  IO functions  ////
 ////////////////////////
 namespace mp {
-    inline void fwrite_msg(FILE* file, std::string_view msg) noexcept {
-        std::fwrite(msg.data(), 1, msg.size(), file);
-    }
+inline void fwrite_msg(FILE* file, std::string_view msg) noexcept {
+    std::fwrite(msg.data(), 1, msg.size(), file);
+}
+
+inline void debug_write(FILE* file, std::string_view msg) noexcept {
+    std::setvbuf(file, NULL, _IONBF, 0);
+    fwrite_msg(file, msg);
+}
 } // namespace mp
