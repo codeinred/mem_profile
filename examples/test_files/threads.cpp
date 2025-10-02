476701f2d6616add91564ab35d567ae4ce55766f
Alecto Irene Perez <alecto.perez@gmail.com>
2025-10-01 23:58:03 -0400

    simplify threads example, move more complex one to threads2

--- examples/test_files/threads.cpp
+++ examples/test_files/threads.cpp
@@ -1,35 +1,11 @@
-#include <array>
 #include <thread>
-#include <vector>
-
-using bytes = std::vector<std::byte>;
-
-template <size_t I>
-struct my_object {
-    bytes a = bytes(10);
-    bytes b = bytes(100);
-    bytes c = bytes(1000);
-};
-
-template <size_t I>
-auto do_stuff(size_t count, size_t num_iters) {
-    return [count, num_iters] {
-        for (size_t i = 0; i < num_iters; i++) {
-            std::vector<my_object<I>> v(count);
-        }
-    };
-}
+#include <cstdio>
 
 
 int main() {
-    std::array<std::thread, 4> arr{
-        std::thread(do_stuff<0>(100, 10)),
-        std::thread(do_stuff<1>(100, 10)),
-        std::thread(do_stuff<2>(100, 10)),
-        std::thread(do_stuff<3>(100, 10)),
-    };
+    auto t = std::thread([] {
+        puts("Running from thread");
+    });
 
-    for (auto& thread : arr) {
-        thread.join();
-    }
+    t.join();
 }
